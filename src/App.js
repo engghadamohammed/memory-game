b368afd00b5cbb6408599a613dc4c8efedf38f0b
ghada <100215621+engghadamohammed@users.noreply.github.com>
2023-03-19 20:43:23 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MemoryGame from "./components/MemoryGame";
+
+function App() {
+	return (
+		
+		<div className="container my-2">
+			<MemoryGame />
+			</div>
+			
+	);
+}
+
+export default App;
---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+export const images = [
+	"https://cdn.vectorstock.com/i/preview-1x/39/91/animal-alphabet-d-vector-29733991.webp",
+	"https://thumbs.dreamstime.com/b/vector-fruit-alphabet-education-illustration-kids-letter-apple-vector-fruit-alphabet-education-illustration-121327408.jpg",
+	"https://us.123rf.com/450wm/natchapohn/natchapohn1805/natchapohn180500103/103764335-lettera-c-carino-bambini-trasporti-colorati-abc-alfabeto-flashcard-di-auto-per-bambini-che-imparano.jpg?ver=6",
+	"https://cdn.vectorstock.com/i/preview-1x/20/59/alphabet-printable-flashcard-with-letter-f-vector-43812059.webp",
+	"https://cdn.vectorstock.com/i/preview-1x/96/42/character-s-cheerful-sheep-on-abc-for-children-vector-15009642.webp",
+	"https://cdn.vectorstock.com/i/preview-lt/16/57/english-animals-zoo-alphabet-letter-g-vector-10921657.webp",
+	"https://cdn5.vectorstock.com/i/thumbs/17/54/english-animals-zoo-alphabet-letter-w-vector-10921754.jpg",
+	"https://cdn.vectorstock.com/i/preview-1x/09/69/h-consonant-demonstration-with-horse-character-vector-22950969.webp",
+	"https://cdn.vectorstock.com/i/preview-1x/92/98/alphabet-printable-flashcard-with-letter-o-and-owl-vector-43819298.webp",
+	"https://cdn.vectorstock.com/i/preview-1x/19/98/cartoon-roaring-taliped-bear-representing-b-letter-vector-23061998.webp",
+
+	
+    
+];
